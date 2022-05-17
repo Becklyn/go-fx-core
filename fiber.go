2e6c2b1019098afbb9921e0ef38da31de4f98016
Samuel Oechsler <so@becklyn.com>
2022-05-17 11:59:30 +0200

    Add ability to change fiber addr via env var

--- fiber.go
+++ fiber.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"context"
+
+	"github.com/Becklyn/go-fx-core/env"
 	"github.com/gofiber/fiber/v2"
 	fiberlog "github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/sirupsen/logrus"
@@ -13,6 +15,8 @@ var FiberModule = fx.Provide(
 )
 
 func newFiber(lifecycle fx.Lifecycle, logger *logrus.Logger) *fiber.App {
+	addr := env.StringWithDefault("FIBER_ADDR", ":3000")
+
 	app := fiber.New()
 
 	app.Use(fiberlog.New(fiberlog.Config{
@@ -27,7 +31,7 @@ func newFiber(lifecycle fx.Lifecycle, logger *logrus.Logger) *fiber.App {
 	lifecycle.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			go func() {
-				if err := app.Listen(":3000"); err != nil {
+				if err := app.Listen(addr); err != nil {
 					logger.Fatal(err)
 				}
 			}()

b917cffeb5b1880f1db400219a6d39a7c9d8c097
Samuel Oechsler <so@becklyn.com>
2022-05-23 16:33:51 +0200

    Improve fiber prometheus middleware

--- metrics/fiber_mectrics_middleware.go
+++ metrics/fiber_mectrics_middleware.go
@@ -2,21 +2,25 @@ package metrics
 
 import (
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/Becklyn/go-fx-core/web"
 	"github.com/gofiber/fiber/v2"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.uber.org/fx"
 )
 
-var currentRequests = prometheus.NewGauge(
+var currentRequests = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "fiber_requests_current",
 		Help: "The current number of active requests",
 	},
+	[]string{"method", "path"},
 )
 
-var totalRequests = prometheus.NewCounterVec(
+var totalRequests = promauto.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "fiber_requests_total",
 		Help: "Total number of requests processed by fiber",
@@ -24,47 +28,19 @@ var totalRequests = prometheus.NewCounterVec(
 	[]string{"status", "method", "path"},
 )
 
-var requestDuration = prometheus.NewHistogram(
+var requestDuration = promauto.NewHistogramVec(
 	prometheus.HistogramOpts{
 		Name: "fiber_request_duration_seconds",
 		Help: "Duration of requests processed by fiber",
 		Buckets: []float64{
-			0.000000001, // 1ns
-			0.000000002,
-			0.000000005,
-			0.00000001, // 10ns
-			0.00000002,
-			0.00000005,
-			0.0000001, // 100ns
-			0.0000002,
-			0.0000005,
-			0.000001, // 1µs
-			0.000002,
-			0.000005,
-			0.00001, // 10µs
-			0.00002,
-			0.00005,
-			0.0001, // 100µs
-			0.0002,
-			0.0005,
-			0.001, // 1ms
-			0.002,
-			0.005,
-			0.01, // 10ms
-			0.02,
-			0.05,
-			0.1, // 100 ms
-			0.2,
+			0.1, // Instant response
+			0.25,
 			0.5,
-			1.0, // 1s
-			2.0,
-			5.0,
-			10.0, // 10s
-			15.0,
-			20.0,
-			30.0,
+			0.75,
+			1, // Maximum acceptable limit
 		},
 	},
+	[]string{"status", "method", "path"},
 )
 
 type FiberMetricsMiddleware struct {
@@ -75,32 +51,48 @@ func useFiberMetricsMiddleware(
 	registry *web.FiberMiddlewareRegistry,
 	middleware FiberMetricsMiddleware,
 ) {
-	registry.Use(web.FiberMiddleware{
+	registry.Use(&web.FiberMiddleware{
 		Name:    "Fiber metrics",
 		Handler: middleware.Handler,
 	})
 }
 
 func (m *FiberMetricsMiddleware) Handler(ctx *fiber.Ctx) error {
-	requestDurationTimer := prometheus.NewTimer(requestDuration)
-	defer requestDurationTimer.ObserveDuration()
+	start := time.Now()
+	method := string(ctx.Context().Method())
+	path := string(ctx.Context().Path())
 
-	currentRequests.Inc()
-	defer currentRequests.Dec()
-
-	if err := ctx.Next(); err != nil {
-		return err
+	if strings.Contains(path, "metrics") || strings.Contains(path, "health") {
+		return ctx.Next()
 	}
 
+	currentRequests.WithLabelValues(
+		method,
+		path,
+	).Inc()
+	defer currentRequests.WithLabelValues(
+		method,
+		path,
+	).Dec()
+
+	err := ctx.Next()
 	status := strconv.Itoa(ctx.Response().StatusCode())
-	method := ctx.Route().Method
-	path := ctx.Route().Path
+	if err, ok := err.(*fiber.Error); err != nil && ok {
+		status = strconv.Itoa(err.Code)
+	}
 
 	totalRequests.WithLabelValues(
 		status,
 		method,
 		path,
 	).Inc()
+
+	duration := time.Since(start).Seconds()
+	requestDuration.WithLabelValues(
+		status,
+		method,
+		path,
+	).Observe(duration)
 
 	return nil
 }
